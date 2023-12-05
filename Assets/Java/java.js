85254ec9aee98a4876d21153db25a28f0f9b3118
LeesaM95 <146777629+LeesaM95@users.noreply.github.com>
2023-12-04 22:23:41 -0500

    got flickr api working, working on wiki api next

--- Assets/Java/java.js
+++ Assets/Java/java.js
@@ -2,21 +2,22 @@
 var searchBar = document.getElementById("search-bar");
 var featuredStar = document.getElementById("featured-star");
 var srchBtn = document.getElementById("search-button");
+var inputVal = document.getElementById("submitInput");
 
 var apiKeyFlickr = 'c4aaaedd99b8d8b5a0ee032443cea286';
 var flickrData = {
   method: 'flickr.photos.search',
   api_key: apiKeyFlickr,
-  text: ' ', // Search Text
+  text: " ", // Search Text
   content_types: 0,
+  privacy_filter: 1,
   geo_context: 2,
   per_page: 3,
   extras: 'owner_name,license',
   format: 'json',
   nojsoncallback: 1,
 };
-var flickrParameters = new URLSearchParams(flickrData);
-var flickrUrl = `https://api.flickr.com/services/rest/?${flickrParameters}`;
+
 
 var apiKeyWiki = 'fd4bfdb503018e4083550a93176323b0'
 var wikiData = {
@@ -47,34 +48,34 @@ var wikiUrl = `http://en.wikipedia.org/w/api.php${wikiParameters}`;
 
 
 // TWO B: DEFINE SEARCH FUNCTION FOR FLICKR
+function flickrImgSearch(event) {
+    event.preventDefault();
+    flickrData.text = inputVal.value
+    var flickrParameters = new URLSearchParams(flickrData);
+    var flickrUrl = `https://api.flickr.com/services/rest/?${flickrParameters}`;
 fetch(flickrUrl) 
-  .then(response => response.json())
-  .then(data => {
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (data) {
     console.log(data)
-    // We will impliment something later...
-    
-            
+    // We will impliment something later...        
+    // We want users to be able to input a SEARCH TERM for a star/constellation
+    for (var i = 0; i < data.photos.photo.length; i++) {
+        // creating styling for photo to sit nicely within the code
+        console.log(data.photos.photo[i].id);
+        var imgTag = document.createElement('img');
+        var serverid = data.photos.photo[i].server;
+        var id = data.photos.photo[i].id;
+        var secret = data.photos.photo[i].secret;
+        
+        imgTag.src = `https://live.staticflickr.com/${serverid}/${id}_${secret}.jpg`;
+
+        featuredStar.appendChild(imgTag); 
+    }
   });
+}
 
-//   const getFlickrImageURL = (photo, size) => {
-//     let url = `https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${
-//       photo.secret
-//     }`;
-//     if (size) {
-//       // Configure image size
-//       url += `_${size}`;
-//     }
-//     url += '.jpg';
-//     return url;
-//   };
-  
-//   axios.get(url)
-//     .then(response => response.json())
-//     .then(data => (
-//       // get an array of image-url
-//       data.photos.photo.map((photo) => {
-//         return getFlickrImageURL(photo, 'q');
-//       })
-//     ));
-
+srchBtn.addEventListener('click', flickrImgSearch);
 // // FOR FLICKR //
